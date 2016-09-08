ef98a33bfc7c2234a35ddde9d2a52dbb5710bcd8
Your Name <you@example.com>
2016-09-08 08:51:35 -0400

    5

--- src/AppBundle/Controller/SretniController.php
+++ src/AppBundle/Controller/SretniController.php
@@ -8,6 +8,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class SretniController extends Controller
 {
     /**
+     * @Route("sretni")
+     * @Route("sretni/index")
+     */
+    public function indexAction()
+    {
+        // redirect to the "homepage" route
+        // return $this->redirectToRoute('homepage');
+
+        // do a permanent - 301 redirect
+        // return $this->redirectToRoute('homepage', array(), 301);
+
+        // redirect to a route with parameters
+        // return $this->redirectToRoute('blog_show', array('slug' => 'my-page'));
+
+        // redirect externally
+        // return $this->redirect('http://symfony.com/doc');
+
+        return new Response(
+            '<html><body>Placeholder</body></html>'
+        );
+    }
+
+    /**
      * @Route(
      *     "/sretni/{_locale}/{year}/{title}",
      *     requirements={
