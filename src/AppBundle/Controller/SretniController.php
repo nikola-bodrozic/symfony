b8ada0092ec60db2ab19c3254e268e895aa974a4
Your Name <you@example.com>
2016-09-11 07:29:42 -0400

    8

--- src/AppBundle/Controller/SretniController.php
+++ src/AppBundle/Controller/SretniController.php
@@ -8,14 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SretniController extends Controller
 {
-    /**
-    * @Route("sretni/odgovor")
-    */
-    public function odgovorAction(){
-        $response = new Response(json_encode(array('name' => 'mike')));
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
-    }
+
 
     /**
      * @Route("sretni")
@@ -26,7 +19,7 @@ class SretniController extends Controller
         $session = $request->getSession();
         $session->set('param-name', 'param vrednost');
 
-        return $this->render('sretni/ind.html.twig');
+        return $this->render('sretni/index.html.twig');
 
         // redirect to the "homepage" route
         // return $this->redirectToRoute('homepage');
@@ -41,6 +34,15 @@ class SretniController extends Controller
         // return $this->redirect('http://symfony.com/doc');
 
 
+    }
+
+    /**
+    * @Route("sretni/odgovor")
+    */
+    public function odgovorAction(){
+        $response = new Response(json_encode(array('name' => 'mike')));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
     /**
---
+++ web/js/sretni-javascript.js
@@ -0,0 +1,1 @@
+alert('js fajl za sretni/index ucitan');
