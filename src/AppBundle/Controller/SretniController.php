79d968c2b9aeab1bc36159411a1971d298340ca6
Your Name <you@example.com>
2016-09-14 19:33:56 -0400

    9

--- src/AppBundle/Controller/SretniController.php
+++ src/AppBundle/Controller/SretniController.php
@@ -1,7 +1,6 @@
 <?php
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,6 +8,24 @@ use Symfony\Component\HttpFoundation\Response;
 class SretniController extends Controller
 {
 
+
+    /**
+     * @Route ("sretni/redir")
+     *
+     */
+    public function redirAction(){
+        //redirect to the "homepage" route
+        return $this->redirectToRoute('odgovor');
+
+        // do a permanent - 301 redirect
+        // return $this->redirectToRoute('homepage', array(), 301);
+
+        // redirect to a route with parameters
+        // return $this->redirectToRoute('blog_show', array('slug' => 'my-page'));
+
+        // redirect externally
+        // return $this->redirect('http://symfony.com/doc');
+    }
 
     /**
      * @Route("sretni")
@@ -20,26 +37,13 @@ class SretniController extends Controller
         $session->set('param-name', 'param vrednost');
 
         return $this->render('sretni/index.html.twig');
-
-        // redirect to the "homepage" route
-        // return $this->redirectToRoute('homepage');
-
-        // do a permanent - 301 redirect
-        // return $this->redirectToRoute('homepage', array(), 301);
-
-        // redirect to a route with parameters
-        // return $this->redirectToRoute('blog_show', array('slug' => 'my-page'));
-
-        // redirect externally
-        // return $this->redirect('http://symfony.com/doc');
-
-
     }
 
     /**
-    * @Route("sretni/odgovor")
+    * @Route("sretni/odgovor", name="odgovor")
     */
     public function odgovorAction(){
+        dump(array("nyc", "la"));
         $response = new Response(json_encode(array('name' => 'mike')));
         $response->headers->set('Content-Type', 'application/json');
         return $response;
